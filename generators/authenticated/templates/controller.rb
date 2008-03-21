5c6237983a6609770d1dc604f12ca193ae5087c8
Jens-Christian Fischer <jcf@invisible.ch>
2008-03-21 10:47:58 +0100

    * skip before_filter login_required in controlle template

--- generators/authenticated/templates/controller.rb
+++ generators/authenticated/templates/controller.rb
@@ -2,7 +2,8 @@
 class <%= controller_class_name %>Controller < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-
+  skip_before_filter :login_required
+  
   # render new.rhtml
   def new
   end
